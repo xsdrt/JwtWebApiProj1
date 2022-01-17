6fc23b04ce86084b4693458ea391645a39a135e9
xsdrt <m_redinger@hotmail.com>
2022-01-17 13:32:05 -0500

    Just made the users PasswordHash and Salt Nullable...

--- JwtWebApiTutorial/User.cs
+++ JwtWebApiTutorial/User.cs
@@ -4,7 +4,7 @@
     {
         public string Username { get; set; } = string.Empty;        //Again you would want a user Id and store the user's name and Id in a database in a complete app.
 
-        public byte[] PasswordHash { get; set; }            //So we won't store the password in plain text so we need a byte array with a PasswordHash and PasswordSalt
-        public byte[] PasswordSalt { get; set; }
+        public byte[]? PasswordHash { get; set; }            //So we won't store the password in plain text so we need a byte array with a PasswordHash and PasswordSalt
+        public byte[]? PasswordSalt { get; set; }           //Declare as nullable....
     }
 }
